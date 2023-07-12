a235acf50ec0a79dbcb133617c00bf2b043d51a2
HaVH <havh.ous@mcredit.com.vn>
2023-07-12 15:05:10 +0700

    no message

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const admin = require("firebase-admin");
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
+
+const serviceAccountPath = path.resolve(__dirname, "serviceAccountKey.json");
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT} `);
+});
 
-app.get('/', (req, res) => {
-  res.sendStatus(200)
-})
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountPath),
+});
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
+
+app.post("/send-notification", (req, res) => {
+  const message = {
+    notification: {
+      title: "New Notification",
+      body: "This is a test notification.",
+    },
+    token: req.query.token, // Replace with your desired topic or device token
+  };
+
+  admin
+    .messaging()
+    .send(message)
+    .then((response) => {
+      console.log("Notification sent successfully:", response);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log("Error sending notification:", error);
+      res.sendStatus(500);
+    });
+});
 
 // Export the Express API
-module.exports = app
+module.exports = app;
